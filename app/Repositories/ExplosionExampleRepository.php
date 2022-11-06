523c78093eba33f82ad86b82869533d681326bf8
Arthur <arthur.lord.frost@gmail.com>
2022-11-06 22:36:48 +0200

    Changes by Derlados

--- app/Repositories/ExplosionExampleRepository.php
+++ app/Repositories/ExplosionExampleRepository.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Builder;
 
 class ExplosionExampleRepository
 {
+    private static $YIELD_RANGE = 10;
+
     public function getExamplesByClassification(ExplosionClassification $classification)
     {
         return $this->query()
@@ -16,6 +18,20 @@ class ExplosionExampleRepository
             ->orderBy('power')
             ->get()->toArray();
     }
+
+    //TODO
+    public function getExamplesByYield(int $yeild)
+    {
+        $left_power = max($yeild - ExplosionExampleRepository::$YIELD_RANGE, 0);
+        $right_power = $yeild + ExplosionExampleRepository::$YIELD_RANGE;
+
+        return $this->query()
+            ->where('power','<', $right_power)
+            ->where('power','>=', $left_power)
+            ->orderBy('power')
+            ->get()->toArray();
+    }
+
 
     private function query(): Builder
     {
